c448a46a38c2be5ba3edd23475bddfdd7cd4b87d
matheuspolachini <matheuspolachini@outlook.com>
2022-11-12 17:44:27 -0300

    Display user chirps on the index page

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -14,7 +14,10 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        return view('chirps.index');
+        $chirps = Chirp::with('user')->latest()->get();
+        return view('chirps.index', [
+            'chirps' => $chirps,
+        ]);
     }
 
     /**
